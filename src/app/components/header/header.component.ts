f416a304650a8b9bac4c6415035f9a7d28dda331
haris103 <haris.mahmood103@gmail.com>
2022-11-15 16:10:30 +0500

    dashboard screen, specific header colors

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,6 +9,7 @@ import { Location } from '@angular/common';
 
 export class HeaderComponent implements OnInit {
 @Input() title: any;
+@Input() color: any;
   constructor(
     private location: Location
   ) { }
