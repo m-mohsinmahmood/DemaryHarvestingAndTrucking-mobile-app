abb41056daf4ca255be19e5f6bf3a123ab39c6c8
Umair Ali <uali@cinnova.com>
2023-01-25 13:54:56 +0500

    Routing Training Evaluation Form

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,34 +7,38 @@ import { Router } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-
 export class HeaderComponent implements OnInit {
   @Input() title: any;
   @Input() color: any;
-  constructor(
-    private location: Location,
-    private router: Router
-  ) { }
+  constructor(private location: Location, private router: Router) {}
 
-  ngOnInit() {
-    // console.log('Router:',this.router.getCurrentNavigation());
-    //  console.log('Location',this.location);
-
-  }
-
+  ngOnInit() {}
 
   goBack() {
     // conditions for pre-check-form pages to go back on specific page
-    if(this.location.path() === '/tabs/home/harvesting/complete-pre-check-form/pre-trip-form'){
-      this.router.navigateByUrl('/tabs/home/harvesting/complete-pre-check-form');
-    }else if(this.location.path() === '/tabs/home/harvesting/complete-pre-check-form'){
+    if (
+      this.location.path() ===
+      '/tabs/home/harvesting/complete-pre-check-form/pre-trip-form'
+    ) {
+      this.router.navigateByUrl(
+        '/tabs/home/harvesting/complete-pre-check-form'
+      );
+    } else if (
+      this.location.path() === '/tabs/home/harvesting/complete-pre-check-form'
+    ) {
       this.router.navigateByUrl('/tabs/home/harvesting');
-    }else if(this.location.path() === '/tabs/home/harvesting'){
+    } else if (this.location.path() === '/tabs/home/harvesting') {
       this.router.navigateByUrl('/tabs/home');
-    }
-    else{
-      // for normal navigation (go-back) of all pages
-      this.location.back();
+    } else {
+      if (
+        this.location.path().includes('training/trainer/basic-skills') ||
+        this.location.path().includes('training/trainer/road-skills') ||
+        this.location.path().includes('training/trainer/pre-trip')
+      ) {
+        this.router.navigateByUrl('/tabs/home/training/trainer');
+      } else {
+        this.location.back();
+      }
     }
   }
 }
