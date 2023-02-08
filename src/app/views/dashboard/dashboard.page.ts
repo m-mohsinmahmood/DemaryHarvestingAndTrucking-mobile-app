4e1743653f2bf50ea03dd1478372c35dd18ce478
haris103 <haris.mahmood103@gmail.com>
2023-02-08 17:03:52 +0500

    updated code :)

--- src/app/views/dashboard/dashboard.page.ts
+++ src/app/views/dashboard/dashboard.page.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { CheckInOutService } from './../../components/check-in-out/check-in-out.service';
@@ -40,7 +41,7 @@ export class DashboardPage implements OnInit {
       select: ['']
     });
     this.selectform.get('select').setValue(this.roleOptions[0]);
-    console.log(localStorage.getItem("employeeId"));
+    console.log(localStorage.getItem('employeeId'));
 
     this.dwrServices.getDWR(localStorage.getItem('employeeId')).subscribe(workOrder => {
       console.log(workOrder.dwr);
@@ -53,7 +54,7 @@ export class DashboardPage implements OnInit {
       else {
         this.isModalOpen = true;
       }
-    })
+    });
   }
 
   onSelect(e) {
@@ -77,7 +78,7 @@ export class DashboardPage implements OnInit {
     this.isOpen = true;
   }
   logout() {
-    console.log('LOGOUT')
+    console.log('LOGOUT');
 
     //to close pop-over
     this.isOpen = false;
@@ -88,17 +89,17 @@ export class DashboardPage implements OnInit {
     if (this.activeDwr[0].module === 'farming') {
       this.isModalOpen = false;
       console.log(this.isModalOpen);
-      this.nav.navigate(['farming'], { relativeTo: this.activatedRoute })
+      this.nav.navigate(['farming'], { relativeTo: this.activatedRoute });
     }
     else if (this.activeDwr[0].module === 'trucking') {
       this.isModalOpen = false;
       console.log(this.isModalOpen);
-      this.nav.navigate(['trucking'], { relativeTo: this.activatedRoute })
+      this.nav.navigate(['trucking'], { relativeTo: this.activatedRoute });
     }
     else if (this.activeDwr[0].module === 'harvesting') {
       this.isModalOpen = false;
       console.log(this.isModalOpen);
-      this.nav.navigate(['harvesting'], { relativeTo: this.activatedRoute })
+      this.nav.navigate(['harvesting'], { relativeTo: this.activatedRoute });
     }
   }
 }
