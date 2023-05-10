54623a33b5d3e60d08396d2b1a0e48d81c787350
mohsinmahmood-cinnova <mmahmood@cinnova.com>
2023-05-11 00:21:33 +0500

    One ticket per DWR in Other Module

--- src/app/views/dashboard/others/others.services.ts
+++ src/app/views/dashboard/others/others.services.ts
@@ -19,13 +19,12 @@ export class OthersService {
   constructor(
     private httpClient: HttpClient,
     private alertSerice: AlertService
-  ) {}
+  ) { }
 
   getEmployees(search: any, role) {
     let params = new HttpParams();
     params = params.set('entity', 'allEmployees');
-    // params = params.set('entityType', entityType);
-    params = params.set('role',role);
+    params = params.set('role', role);
     params = params.set('search', search);
     return this.httpClient
       .get<any>('api-1/dropdowns', {
@@ -48,36 +47,38 @@ export class OthersService {
   getOthers(data: any, dwr_type: string): Observable<any> {
     data.dwr_type = dwr_type;
     return this.httpClient
-        .post<any>(`http://api-1/dwr`,data)
-        .pipe(take(1));
+      .post<any>(`http://api-1/dwr`, data)
+      .pipe(take(1));
   }
-save(data, entity: any){
-  let params=  new HttpParams();
+
+  save(data, entity: any) {
+    let params = new HttpParams();
     params = params.set('entity', entity);
     return this.httpClient
-    .post<any>('api-1/other', data,{
-      params
-    })
-    .pipe(take(1));
-}
+      .post<any>('api-1/other', data, {
+        params
+      })
+      .pipe(take(1));
+  }
+
   createDWR(
     employeeId: any,
     other_record_id: any,
     supervisor_id: any,
     dwrId,
-    taskType: any){
+    taskType: any) {
     let data;
 
-       data = {
-        dwr_type: 'other',
-        module: 'other',
-        employeeId,
-        supervisor_id,
-        dwrId,
-        other_record_id,
-        taskType
+    data = {
+      dwr_type: 'other',
+      module: 'other',
+      employeeId,
+      supervisor_id,
+      dwrId,
+      other_record_id,
+      taskType
     };
-    console.log('DATA:',data);
+    console.log('DATA:', data);
     return this.httpClient
       .post<any>(`api-1/dwr`, data)
       .pipe(take(1));
